a34ef8b7ae0d9d684754b9ce07782bea686039c8
zhangyuhao01 <zhangyuhao01@qianxin.com>
2021-02-05 01:17:03 +0800

    delete compiled js

--- src/middleware/dist/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function errorMiddleware(error, request, response, next) {
-    var status = error.status;
-    var message = error.message;
-    response.status(status).send({ status: status, message: message });
-}
-exports["default"] = errorMiddleware;
